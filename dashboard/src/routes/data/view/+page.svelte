83f7811f63f60f420c7f170917898c5191c07ff0
Linus Thorsell <thorsell.linus@gmail.com>
2025-11-11 22:42:07 +0100

    WIP/EXP: experimenting with data fetching for future data fetching utility

--- dashboard/src/routes/data/view/+page.svelte
+++ dashboard/src/routes/data/view/+page.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
     import type { PageProps } from "./$types";
+    import type { ColumnDef } from "@tanstack/table-core";
+
+    import DataTable from "$lib/custom/DataTable.svelte";
+    import * as Card from "$lib/components/ui/card/index.js";
 
     let { data }: PageProps = $props();
+
+    const getColumnDefs = (columns: Record<string, string>): ColumnDef<Record<string, unknown>>[] => {
+        const keys = Object.keys(columns);
+        let defs = keys.map((key: string) => ({
+            accessorKey: key,
+            header: key,
+        }));
+        return defs;
+    };
 </script>
 
 {#each data.tables as table}
-    <div class="mt-2">
-        <span class="font-semibold">{table.table}</span>
-        <ul class="ml-4 list-disc">
-            {#each Object.entries(table.columns) as [column, type]}
-                <li>{column}: <span class="text-slate-400">{type}</span></li>
-            {/each}
-        </ul>
-    </div>
+    <Card.Root>
+        <Card.Header>{table.table}</Card.Header>
+        <Card.Content>
+            <DataTable
+                data={table.rows}
+                columns={getColumnDefs(table.columns)}
+            />
+        </Card.Content>
+    </Card.Root>
 {/each}
--- dashboard/src/routes/data/view/+page.ts
+++ dashboard/src/routes/data/view/+page.ts
@@ -1,10 +1,33 @@
-import type { PageLoad } from './$types';
-import { PUBLIC_API_URL } from '$env/static/public';
+import type { PageLoad } from "./$types";
+import { PUBLIC_API_URL } from "$env/static/public";
 
-export const load: PageLoad = async () => {
-    let tables = await fetch(`${PUBLIC_API_URL}/database/table`);
+type TableDef = {
+    table: string;
+    columns: Record<string, string>;
+    rows: Record<string, unknown>[];
+};
+
+export const load: PageLoad = async ({ fetch }) => {
+    const tablesRes = await fetch(`${PUBLIC_API_URL}/database/table`);
+    const baseTables = (await tablesRes.json()) as Array<
+        Omit<TableDef, "rows">
+    >;
+
+    const tables: TableDef[] = await Promise.all(
+        baseTables.map(async (table) => {
+            const rowsRes = await fetch(
+                `${PUBLIC_API_URL}/collection/${table.table}`,
+            );
+            const rows = (await rowsRes.json()) as Record<string, unknown>[];
+            return {
+                ...table,
+                rows,
+            };
+        }),
+    );
 
     return {
-        tables: await tables.json()
+        tables,
     };
 };
+
